d50467fb261f1f5fbe6066a1aed4435a80c41389
jfreitas <jfreitas@student.s19.be>
2021-01-13 04:40:38 +0100

    doing a correction part

--- ft_services_updated/srcs/services/mysql/create_db.sql
+++
@@ -1,6 +0,0 @@
-FLUSH PRIVILEGES;
-CREATE DATABASE wordpress;
-CREATE USER 'user'@'%' IDENTIFIED BY 'password';
-GRANT ALL ON wordpress.* TO 'user'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
-DROP DATABASE test; /*The test database that was automatically created has to be removed*/
-FLUSH PRIVILEGES;
--- ft_services_updated/srcs/services/telegraf/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm /telegraf-1.13.3-static_linux_amd64.tar.gz
-rm -rf /telegraf
-
-/usr/bin/telegraf
