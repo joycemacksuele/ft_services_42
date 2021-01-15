a6f3a4d68af7dc5be1c57ad07551e1e8a9659080
jfreitas <jfreitas@student.s19.be>
2021-01-15 22:21:52 +0100

    DONE 2.0

--- ft_services_vm_updated/srcs/services/mysql/create_db.sql
+++
@@ -1,7 +0,0 @@
-FLUSH PRIVILEGES;
-/*CREATE DATABASE wordpress;*/
-CREATE DATABASE IF NOT EXISTS wordpress CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'user'@'%' IDENTIFIED BY 'password';
-GRANT ALL ON wordpress.* TO 'user'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
-DROP DATABASE test; /*The test database that was automatically created has to be removed*/
-FLUSH PRIVILEGES;
